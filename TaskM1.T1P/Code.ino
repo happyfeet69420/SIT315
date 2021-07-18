8afd5fd99eaaf0909a69bd0abaa4e4035c32ec02
happyfeet69420 <81469270+happyfeet69420@users.noreply.github.com>
2021-07-18 15:55:39 +1000

    Create Code.ino

---
+++ TaskM1.T1P/Code.ino
@@ -0,0 +1,31 @@
+int sensorPin = 0;
+int ledPin = 13;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int reading = analogRead(sensorPin); 
+  
+  float volt = reading * 5.0;
+  volt /= 1024.0;
+  
+  float tempature = (volt - 0.5) * 100; 
+  Serial.print(tempature);
+  Serial.println(" degrees C");
+  
+  if (tempature > 30)
+  {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED is on");
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED is off");
+  }
+}
